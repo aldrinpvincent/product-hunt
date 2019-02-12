49a8630ced62a76fe3d3d801595a5748db28ef2e
Aldrin P Vincent <aldrinpvincent@gmail.com>
2019-02-12 23:32:21 +0530

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <header className="App-header">
+          <p>Product Hunt Start</p>
+        </header>
+      </>
+    );
+  }
+}
+
+export default App;
